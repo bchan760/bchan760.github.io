4eebb9cf1ab18da0c6a41b327a1e812f80840fd2
Bill <bchan760@gmail.com>
2019-03-18 18:00:48 -0700

    update

--- nineFlowersChallenge.js
+++ nineFlowersChallenge.js
@@ -1,5 +1,14 @@
-let flower = svg('circle')
-let bee = svg('oval')
-function flower(){
-    
+let draw = SVG('canvas').size(800,800);
+let circWidth = 75;
+let circHeight = 75;
+
+function drawFlower(x, y){
+    let circ = draw.circle(50, 50).fill('white').move(x+50,x+50);
+    var line = draw.line(0, 100, 0, 50).move(x+75, x+100)
+    line.stroke({ color: 'green', width: 7.5, linecap: 'round' })
 }
+
+drawFlower(0, 0);
+drawFlower(200, 0);
+drawFlower(600, 100);
+
